75779c4413b690244fcd4cbc42526aee2f149f89
luxuze <luxuze@icloud.com>
2020-11-24 15:28:55 +0800

    leetcode 513

--- leetcode/leetcode_test.go
+++ leetcode/leetcode_test.go
@@ -4,7 +4,16 @@ import (
 	"testing"
 )
 
+var tree = &TreeNode{
+	Val: 1,
+	Left: &TreeNode{
+		Val: 2,
+	},
+	Right: &TreeNode{
+		Val: 3,
+	},
+}
+
 func TestLeetcode(t *testing.T) {
-	// t.Log(longestCommonPrefix([]string{"baac", "acb", "bacc", "cb"}))
-	t.Log(longestCommonPrefix([]string{"abc", "acb", "c"}))
+	t.Log(findBottomLeftValue(tree))
 }
