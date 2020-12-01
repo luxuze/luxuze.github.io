0555c94ea98d4ec1efa07947dc16f3fad00d065d
luxuze <luxuze@icloud.com>
2020-12-01 22:13:04 +0800

    leetcode 94

---
+++ leetcode/94.二叉树的中序遍历.go
@@ -0,0 +1,98 @@
+package leetcode
+
+// 68/68 cases passed (0 ms)
+// Your runtime beats 100 % of golang submissions
+// Your memory usage beats 94.41 % of golang submissions (2 MB)
+/*
+ * @lc app=leetcode.cn id=94 lang=golang
+ *
+ * [94] 二叉树的中序遍历
+ *
+ * https://leetcode-cn.com/problems/binary-tree-inorder-traversal/description/
+ *
+ * algorithms
+ * Medium (74.25%)
+ * Likes:    792
+ * Dislikes: 0
+ * Total Accepted:    307.2K
+ * Total Submissions: 413.8K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * 给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [1,null,2,3]
+ * 输出：[1,3,2]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = []
+ * 输出：[]
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：root = [1]
+ * 输出：[1]
+ *
+ *
+ * 示例 4：
+ *
+ *
+ * 输入：root = [1,2]
+ * 输出：[2,1]
+ *
+ *
+ * 示例 5：
+ *
+ *
+ * 输入：root = [1,null,2]
+ * 输出：[1,2]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中节点数目在范围 [0, 100] 内
+ * -100
+ *
+ *
+ *
+ *
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func inorderTraversal(root *TreeNode) (answer []int) {
+	var dfs func(*TreeNode)
+	dfs = func(tn *TreeNode) {
+		if tn == nil {
+			return
+		}
+		dfs(tn.Left)
+		answer = append(answer, tn.Val)
+		dfs(tn.Right)
+	}
+	dfs(root)
+	return
+}
+
+// @lc code=end
