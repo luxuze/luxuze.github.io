efc48dee0c867017019ea8db91098ad6dc9a087c
luxuze <luxuze@icloud.com>
2020-12-07 15:08:21 +0800

    leetcode 19

---
+++ leetcode/19.删除链表的倒数第n个节点.go
@@ -0,0 +1,65 @@
+package leetcode
+
+// 208/208 cases passed (0 ms)
+// Your runtime beats 100 % of golang submissions
+// Your memory usage beats 59.78 % of golang submissions (2.2 MB)
+/*
+ * @lc app=leetcode.cn id=19 lang=golang
+ *
+ * [19] 删除链表的倒数第N个节点
+ *
+ * https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/description/
+ *
+ * algorithms
+ * Medium (40.59%)
+ * Likes:    1137
+ * Dislikes: 0
+ * Total Accepted:    291.8K
+ * Total Submissions: 718.9K
+ * Testcase Example:  '[1,2,3,4,5]\n2'
+ *
+ * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+ *
+ * 示例：
+ *
+ * 给定一个链表: 1->2->3->4->5, 和 n = 2.
+ *
+ * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+ *
+ *
+ * 说明：
+ *
+ * 给定的 n 保证是有效的。
+ *
+ * 进阶：
+ *
+ * 你能尝试使用一趟扫描实现吗？
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	slow, fast := head, head
+	for i := 0; i < n; i++ {
+		fast = fast.Next
+	}
+	if fast == nil {
+		head = head.Next
+		return head
+	}
+	for fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next
+	}
+	slow.Next = slow.Next.Next
+	return head
+}
+
+// @lc code=end
