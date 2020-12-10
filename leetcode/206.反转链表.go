39e1e25c385dba15ddbbabbb7a80a89735b4a77f
luxuze <luxuze@icloud.com>
2020-12-10 17:57:56 +0800

    lc 206

--- leetcode/206.反转链表.go
+++ leetcode/206.反转链表.go
@@ -1,12 +1,30 @@
 package leetcode
 
-// 27/27 cases passed (0 ms)
-// Your runtime beats 100 % of golang submissions
-// Your memory usage beats 100 % of golang submissions (2.5 MB)
 /*
  * @lc app=leetcode.cn id=206 lang=golang
  *
  * [206] 反转链表
+ *
+ * https://leetcode-cn.com/problems/reverse-linked-list/description/
+ *
+ * algorithms
+ * Easy (66.04%)
+ * Likes:    1113
+ * Dislikes: 0
+ * Total Accepted:    288.2K
+ * Total Submissions: 412K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * 反转一个单链表。
+ *
+ * 示例:
+ *
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ *
+ * 进阶:
+ * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+ *
  */
 
 // @lc code=start
@@ -17,24 +35,12 @@ package leetcode
  *     Next *ListNode
  * }
  */
-
-// func reverseList(head *ListNode) *ListNode {
-// 	var newHead *ListNode
-// 	for head != nil {
-// 		next := head.Next
-// 		head.Next = newHead
-// 		newHead = head
-// 		head = next
-// 	}
-// 	return newHead
-// }
-
 func reverseList(head *ListNode) *ListNode {
 	if head == nil || head.Next == nil {
 		return head
 	}
-	newHead := reverseList(head.Next)
-	head.Next.Next = head
+	newHead := reverseList(head)
+	head.Next.Next = head.Next
 	head.Next = nil
 	return newHead
 }
