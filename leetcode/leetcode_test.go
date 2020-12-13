5ef88c8113d0d7bb8e4fb02c3fa1d9084e8b1cf8
Monaco <luxuze@icloud.com>
2020-12-13 18:49:46 +0800

    lc 24

--- leetcode/leetcode_test.go
+++ leetcode/leetcode_test.go
@@ -6,12 +6,12 @@ import (
 
 var (
 	tree = Deserialize("3,1,2")
-	ln   = &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4, Next: &ListNode{Val: 5}}}}}
+	ln   = &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3}}}
 	// ln = &ListNode{Val: 3, Next: &ListNode{Val: 4}}
 )
 
 func TestT(t *testing.T) {
-	answer := reverseBetween(ln, 2, 4)
+	answer := swapPairs(ln)
 	t.Log(answer)
 	t.Log(answer.Next)
 	t.Log(answer.Next.Next)
