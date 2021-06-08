022ca709f4566c74e2146beb3b5d7b66f4c7e4c6
卢旭泽 <luxuze@icloud.com>
2021-06-08 08:34:33 +0000

    lc 96

---
+++ leetcode/96.不同的二叉搜索树.go
@@ -0,0 +1,61 @@
+package leetcode
+
+// 19/19 cases passed (0 ms)
+// Your runtime beats 100 % of golang submissions
+// Your memory usage beats 42.77 % of golang submissions (1.9 MB)
+/*
+ * @lc app=leetcode.cn id=96 lang=golang
+ *
+ * [96] 不同的二叉搜索树
+ *
+ * https://leetcode-cn.com/problems/unique-binary-search-trees/description/
+ *
+ * algorithms
+ * Medium (69.62%)
+ * Likes:    1182
+ * Dislikes: 0
+ * Total Accepted:    129.5K
+ * Total Submissions: 186.1K
+ * Testcase Example:  '3'
+ *
+ * 给你一个整数 n ，求恰由 n 个节点组成且节点值从 1 到 n 互不相同的 二叉搜索树 有多少种？返回满足题意的二叉搜索树的种数。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：n = 3
+ * 输出：5
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：n = 1
+ * 输出：1
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1
+ *
+ *
+ */
+
+// @lc code=start
+func numTrees(n int) int {
+	dp := make([]int, n+1)
+	dp[0], dp[1] = 1, 1
+	for i := 2; i <= n; i++ {
+		for j := 1; j <= i; j++ {
+			dp[i] += dp[j-1] * dp[i-j]
+		}
+	}
+	return dp[n]
+}
+
+// @lc code=end
