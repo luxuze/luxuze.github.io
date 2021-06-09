78634decbbef758f5938072e299523b1f0cf46d1
卢旭泽 <luxuze@icloud.com>
2021-06-09 08:47:48 +0000

    lc 203

--- leetcode/leetcode_test.go
+++ leetcode/leetcode_test.go
@@ -6,9 +6,10 @@ import (
 
 var (
 	tree = treeNode.Deserialize("-2,null,-3")
-	ln   = listNode.Deserialize("1,2,3,4")
+	ln   = listNode.Deserialize("1,2,2,1")
 )
 
 func TestT(t *testing.T) {
-	t.Log(hasPathSum(tree, -5))
+	l := removeElements(ln, 2)
+	t.Log(ln.Serialize(l))
 }
