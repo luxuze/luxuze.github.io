0defdeba64ddb14ee28ef2f243451fcd5f564644
卢旭泽 <luxuze@icloud.com>
2021-07-27 06:01:13 +0000

    235

---
+++ leetcode/235.二叉搜索树的最近公共祖先.go
@@ -0,0 +1,79 @@
+package leetcode
+
+// 27/27 cases passed (20 ms)
+// Your runtime beats 86.86 % of golang submissions
+// Your memory usage beats 79.51 % of golang submissions (6.9 MB)
+/*
+ * @lc app=leetcode.cn id=235 lang=golang
+ *
+ * [235] 二叉搜索树的最近公共祖先
+ *
+ * https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+ *
+ * algorithms
+ * Easy (66.50%)
+ * Likes:    622
+ * Dislikes: 0
+ * Total Accepted:    154.3K
+ * Total Submissions: 232K
+ * Testcase Example:  '[6,2,8,0,4,7,9,null,null,3,5]\n2\n8'
+ *
+ * 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+ *
+ * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x
+ * 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ *
+ * 例如，给定如下二叉搜索树:  root = [6,2,8,0,4,7,9,null,null,3,5]
+ *
+ *
+ *
+ *
+ *
+ * 示例 1:
+ *
+ * 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+ * 输出: 6
+ * 解释: 节点 2 和节点 8 的最近公共祖先是 6。
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+ * 输出: 2
+ * 解释: 节点 2 和节点 4 的最近公共祖先是 2, 因为根据定义最近公共祖先节点可以为节点本身。
+ *
+ *
+ *
+ * 说明:
+ *
+ *
+ * 所有节点的值都是唯一的。
+ * p、q 为不同节点且均存在于给定的二叉搜索树中。
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val   int
+ *     Left  *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func lowestCommonAncestor(root, p, q *TreeNode) (ans *TreeNode) {
+	ans = root
+	for {
+		if p.Val < ans.Val && q.Val < ans.Val {
+			ans = ans.Left
+		} else if p.Val > ans.Val && q.Val > ans.Val {
+			ans = ans.Right
+		} else {
+			return ans
+		}
+	}
+}
+
+// @lc code=end
