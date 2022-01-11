9543ecac86f343b65fc6cd1b6d766211d1fed2de
卢旭泽 <luxuze@icloud.com>
2022-01-11 11:00:03 +0000

    405

---
+++ leetcode/405.数字转换为十六进制数.go
@@ -0,0 +1,62 @@
+package leetcode
+
+// 100/100 cases passed (0 ms)
+// Your runtime beats 100 % of golang submissions
+// Your memory usage beats 97.83 % of golang submissions (1.9 MB)
+import "strconv"
+
+/*
+ * @lc app=leetcode.cn id=405 lang=golang
+ *
+ * [405] 数字转换为十六进制数
+ *
+ * https://leetcode-cn.com/problems/convert-a-number-to-hexadecimal/description/
+ *
+ * algorithms
+ * Easy (55.93%)
+ * Likes:    228
+ * Dislikes: 0
+ * Total Accepted:    48.6K
+ * Total Submissions: 86.9K
+ * Testcase Example:  '26'
+ *
+ * 给定一个整数，编写一个算法将这个数转换为十六进制数。对于负整数，我们通常使用 补码运算 方法。
+ *
+ * 注意:
+ *
+ *
+ * 十六进制中所有字母(a-f)都必须是小写。
+ *
+ * 十六进制字符串中不能包含多余的前导零。如果要转化的数为0，那么以单个字符'0'来表示；对于其他情况，十六进制字符串中的第一个字符将不会是0字符。
+ * 给定的数确保在32位有符号整数范围内。
+ * 不能使用任何由库提供的将数字直接转换或格式化为十六进制的方法。
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入:
+ * 26
+ *
+ * 输出:
+ * "1a"
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入:
+ * -1
+ *
+ * 输出:
+ * "ffffffff"
+ *
+ *
+ */
+
+// @lc code=start
+func toHex(num int) string {
+	return strconv.FormatUint(uint64(uint32(num)), 16)
+}
+
+// @lc code=end
